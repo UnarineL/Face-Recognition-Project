935c5de18896eb8ba7214500b5c9b53fc1f0b80e
UnarineL <123364783+UnarineL@users.noreply.github.com>
2024-03-18 16:06:02 +0200

    Update detect_profile.py

--- detect_profile.py
+++ detect_profile.py
@@ -6,7 +6,6 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-#import cv2
 import numpy as np
 
 DATABASE_URL = "sqlite:///faces.db"
@@ -24,7 +23,7 @@ class Face(Base):
 
 Base.metadata.create_all(engine)
 
-#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 
 # Load stored faces
 Session = sessionmaker(bind=engine)
@@ -134,36 +133,7 @@ if not is_streamlit_running():
     streamlit_script_path = "c:/Users/unari/Desktop/Face Recognition Project/detect_profile.py"
     command = ["streamlit", "run", streamlit_script_path]
     subprocess.run(command)
-"""
-if st.button('Face Detect'):
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-        
-        for (x, y, w, h) in faces:
-            face_gray = cv2.resize(gray[y:y+h, x:x+w], (100, 100))  
 
-            recognized = False
-            for stored_face_gray, stored_face in zip(stored_face_gray_list, stored_faces):
-                match_found = np.array_equal(face_gray, stored_face_gray)
-                print(f"Match found: {match_found}, Name: {stored_face.name}")
-
-                if match_found:
-                    recognized = True
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    cv2.putText(frame, f"Name: {stored_face.name}", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
-                    break
-
-            if not recognized:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                cv2.putText(frame, "Not Recognized", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
-
-        cv2.imshow('Face Recognition', frame)
-        key = cv2.waitKey(1)
-        if key == ord('q') or key == 27:  # Press 'q' or Esc to exit
-            break
 
     cap.release()
     cv2.destroyAllWindows()
